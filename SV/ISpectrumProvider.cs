16f75f657451f64a651b57832aa20a1733262b01
DarkT <64225524+D4rkTT@users.noreply.github.com>
2020-05-02 09:29:29 +0200

    Delete ISpectrumProvider.cs

--- SV/ISpectrumProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace WinformsVisualization.Visualization
-{
-    public interface ISpectrumProvider
-    {
-        bool GetFftData(float[] fftBuffer, object context);
-        int GetFftBandIndex(float frequency);
-    }
-}
